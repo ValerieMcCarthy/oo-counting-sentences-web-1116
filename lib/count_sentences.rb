a2a9379115b9fa4d6dcbdf958711c67bd092f4c4
Valerie McCarthy <valeriewilsonmccarthy@gmail.com>
2016-11-30 18:18:16 -0500

    Sentences counted.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,37 @@ require 'pry'
 
 class String
 
+attr_accessor = :sentence
+
+def initialize(sentence)
+	@sentence = sentence
+end
+
   def sentence?
-    
+  	self.end_with?(".")
   end
 
   def question?
-
+	self.end_with?("?")
   end
 
   def exclamation?
-
+  	self.end_with?("!")
   end
 
   def count_sentences
+  	count = 0
+  	new_array = []
+  	new_array = self.split(/\?|\.|!/)
+  	new_array = new_array - [""]
+  	new_array.size
+  	#binding.pry
+  	#new_array.each do |phrase|
+  	#	if phrase 
+  	#		count += 1
+  	#	end
+  	#nd
+  	#count
 
   end
 end
